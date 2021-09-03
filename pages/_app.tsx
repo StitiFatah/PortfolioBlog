89997c585074a93ce1ed0357ffc889e5c56000d8
D_user <ffoot9698@gmail.com>
2021-09-03 09:58:51 +0200

    darkmode_amelioration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,55 +1,64 @@
 import "../styles/global.css";
 import React, { useState, useEffect } from "react";
 
-export default function App({ Component, pageProps }) {
-  const darkbgcolor = "#282C35";
+const darkbgcolor = "#282C35";
 
+const initial_color_mode = () => {
+  // if (typeof window !== "undefined") {
+  const ls_dark_mode = localStorage.getItem("fs_blog_dark_mode");
+  if (ls_dark_mode === "dark") {
+    console.log("initial dark theme");
+    return true;
+  }
+  console.log("initial white theme");
+  return false;
+  // }
+};
+
+const set_styles_to_dark = () => {
+  console.log("set style to dark");
+  document.querySelector("html").style.background = darkbgcolor;
+  document.querySelector("html").classList.add("dark");
+};
+
+const set_styles_to_light = () => {
+  console.log("set style to light");
+
+  document.querySelector("html").style.background = "white";
+  document.querySelector("html").classList.remove("dark");
+};
+
+const set_ls_to = (dark_white) => {
+  localStorage.setItem(
+    "fs_blog_dark_mode",
+    dark_white === "dark" ? "dark" : "light"
+  );
+};
+
+export default function App({ Component, pageProps }) {
   // initial theme
 
-  const initial_color_mode = () => {
-    if (typeof window !== "undefined") {
-      const ls_dark_mode = localStorage.getItem("fs_blog_dark_mode");
-      if (ls_dark_mode === "dark") {
-        console.log("initial dark theme");
-        return true;
-      }
-      console.log("initial white theme");
-      return false;
-    }
-  };
+  const [dark, setDark] = useState(null);
 
-  const [dark, setDark] = useState(initial_color_mode());
-
-  //
-
-  const set_styles_to_dark = () => {
-    document.querySelector("html").style.background = darkbgcolor;
-    document.querySelector("html").classList.add("dark");
-  };
-
-  const set_styles_to_light = () => {
-    document.querySelector("html").style.background = "white";
-    document.querySelector("html").classList.remove("dark");
-  };
-
-  const set_ls_to = (dark_white) => {
-    localStorage.setItem(
-      "fs_blog_dark_mode",
-      dark_white === "dark" ? "dark" : "light"
-    );
-  };
+  useEffect(() => {
+    setDark(initial_color_mode());
+  }, []);
 
   // Reactive
 
   useEffect(() => {
     console.log("App useEffect");
 
-    if (dark) {
-      set_styles_to_dark();
-      set_ls_to("dark");
-    } else {
-      set_styles_to_light();
-      set_ls_to("light");
+    if (dark !== null) {
+      console.log("App useEffect validated");
+
+      if (dark) {
+        set_styles_to_dark();
+        set_ls_to("dark");
+      } else {
+        set_styles_to_light();
+        set_ls_to("light");
+      }
     }
   }, [dark]);
 
